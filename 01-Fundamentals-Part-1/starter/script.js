be1200696003b3701a83c6cce54639148cac22a0
mohamed <mohamedghalib157@gmail.com>
2023-07-27 15:20:10 +0100

    commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,45 @@
-let fDo = parseInt(prompt('Results for first round, Dolfins: '))
-let fKo = parseInt(prompt('Results for first round, Koalas: '))
-let sDo = parseInt(prompt('Results for second round, Dolfins: '))
-let sKo = parseInt(prompt('Results for second round, Koalas: '))
-let tDo = parseInt(prompt('Results for third round, Dolfins: '))
-let tKo = parseInt(prompt('Results for third round, Koalas: '))
+// let fDo = parseInt(prompt('Results for first round, Dolfins: '))
+// let fKo = parseInt(prompt('Results for first round, Koalas: '))
+// let sDo = parseInt(prompt('Results for second round, Dolfins: '))
+// let sKo = parseInt(prompt('Results for second round, Koalas: '))
+// let tDo = parseInt(prompt('Results for third round, Dolfins: '))
+// let tKo = parseInt(prompt('Results for third round, Koalas: '))
 
-let avrDol = ((fDo + sDo + tDo) / 3).toFixed(2);
-let avrKoa = ((fKo + sKo + tKo) / 3).toFixed(2);
-console.log(avrDol, avrKoa)
+// let avrDol = ((fDo + sDo + tDo) / 3).toFixed(2);
+// let avrKoa = ((fKo + sKo + tKo) / 3).toFixed(2);
+// console.log(avrDol, avrKoa)
 
-if (avrDol >= 100 || avrKoa >= 100) {
-    if (avrDol > avrKoa) {
-        console.log('Team Dolphins wins. 🏆');
-    } else if (avrDol === avrKoa) {
-        console.log("Tie. 😵");
-    } else {
-        console.log("Team Koalas wins. 🏆");
-    }
-} else if (avrDol === avrKoa) {
-    console.log("Tie. 😵");
-} else {
-    console.log('no winner. ❌')
-}
+// if (avrDol >= 100 || avrKoa >= 100) {
+//     if (avrDol > avrKoa) {
+//         console.log('Team Dolphins wins. 🏆');
+//     } else if (avrDol === avrKoa) {
+//         console.log("Tie. 😵");
+//     } else {
+//         console.log("Team Koalas wins. 🏆");
+//     }
+// } else if (avrDol === avrKoa) {
+//     console.log("Tie. 😵");
+// } else {
+//     console.log('no winner. ❌')
+// }
+// let language = prompt('enter your language:');
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         alert('MOST number of native speakers!');
+//         break;
+//     case 'spanish':
+//         alert('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         alert('3rd place');
+//         break;
+//     case 'hindi':
+//         alert('Number 4');
+//         break;
+//     case 'arabic':
+//         alert('5th most spoken language');
+//         break;
+//     default:
+//         alert('Great language too :D');
+// }
