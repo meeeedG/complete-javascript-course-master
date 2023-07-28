714ef0c0ed61b6eed6bb2b2b4ae398807bea336b
mohamed <mohamedghalib157@gmail.com>
2023-07-28 17:20:21 +0100

    kh

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 
+// function juice(apples, bananas) {
+//     const choice = `we will add ${apples} apples and ${bananas} bananas to the mix`;
+//     return choice;
+// }
+
+// let a = prompt('how many apples you want?')
+// let b = prompt('how many bananas you want?')
+// console.log(juice(a, b));
