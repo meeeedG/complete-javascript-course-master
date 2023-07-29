c1b586e4accb29bf40782d2b9e7e1e97445b4459
mohamed <mohamedghalib157@gmail.com>
2023-07-29 22:29:02 +0100

    first commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,3 +9,21 @@
 // let a = prompt('how many apples you want?')
 // let b = prompt('how many bananas you want?')
 // console.log(juice(a, b));
+
+// function describeCountry(c, p, cap) {
+//     let info = (`${c} has ${p} million people and its capital city is ${cap}`);
+//     return info;
+// }
+
+// let nameC = prompt("what's your country name?");
+// let popu = prompt("how many population?");
+// let capi = prompt("what is the capital of your country?")
+// console.log(describeCountry(nameC, popu, capi))
+
+function ageCalc(age) {
+    let gage = 2023 - age;
+    return gage;
+}
+
+let givenAge = prompt("What year were you born in?");
+console.log(`you are ${ageCalc(givenAge)} years old.`);
